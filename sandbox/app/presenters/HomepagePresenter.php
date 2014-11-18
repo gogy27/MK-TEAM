439b2eed8de24ef26783928656efc0f915548d0f
Kalino <m.kaluzak@gmail.com>
2014-11-18 17:24:54 +0100

    First changes, route homepage

--- sandbox/app/presenters/HomepagePresenter.php
+++ sandbox/app/presenters/HomepagePresenter.php
@@ -3,18 +3,59 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
-
+    App\Model, Nette\Forms\Form;;
 
 /**
  * Homepage presenter.
  */
-class HomepagePresenter extends BasePresenter
-{
+class HomepagePresenter extends BasePresenter {
 
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
+    const STUDENT = 'S', TEACHER = 'T';
+
+    public function actionDefault() {
+        $user = $this->getUser();
+        if ($user->isLoggedIn()) {
+            if ($user->isInRole(STUDENT)) {
+                $this->redirect('Homepage:student');
+            } else if ($user->isInRole(TEACHER)) {
+                $this->redirect('Homepage:teacher');
+            }
+        }
+    }
+
+    public function renderDefault() {
+        $this->template->anyVariable = 'any value';
+    }
+
+    protected function createComponentNewLoginForm() {
+        $form = new Form();
+        $form->addText('name', 'Meno:')
+                ->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie meno')
+                ->setAttribute('placeholder', 'Zadajte meno');
+        $form->addPassword('password', 'Heslo:')
+                ->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie heslo')
+                ->setAttribute('placeholder', 'Zadajte heslo');
+        $form->addSubmit('login', 'Prihlásiť');
+        $form->onSuccess[] = $this->newLoginFormSubmitted;
+
+        return $form;
+    }
+    
+    public function newLoginFormSubmitted(Form $form) {
+        /*$user = $this->getUser();
+        $values = $form->getValues();
+        $username = $values['name'];
+        $password = $values['password'];
+        $successful = true;
+        try {
+            $user->login($username, $password);
+        } catch (Nette\Security\AuthenticationException $e) {
+            $successful = false;
+            $this->flashMessage($e->getMessage());
+        }
+        if ($successful) {
+            $this->redirect('Homepage:default');
+        }*/
+    }
 
 }
--- sandbox/temp/cache/latte/GitHub-MK-TEAM-sandbox-app-templates-layout-latte-21a578f6c4b3341e21941f979841eacd.php
+++ sandbox/temp/cache/latte/GitHub-MK-TEAM-sandbox-app-templates-layout-latte-21a578f6c4b3341e21941f979841eacd.php
@@ -2,20 +2,20 @@
 // source: C:\Users\Matus\Documents\GitHub\MK-TEAM\sandbox\app/templates/@layout.latte
 
 // prolog Latte\Macros\CoreMacros
-list($_b, $_g, $_l) = $template->initialize('2278820105', 'html')
+list($_b, $_g, $_l) = $template->initialize('6437686456', 'html')
 ;
 // prolog Latte\Macros\BlockMacros
 //
 // block head
 //
-if (!function_exists($_b->blocks['head'][] = '_lbc9f735f61d_head')) { function _lbc9f735f61d_head($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+if (!function_exists($_b->blocks['head'][] = '_lbcb390ea404_head')) { function _lbcb390ea404_head($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
 ;
 }}
 
 //
 // block scripts
 //
-if (!function_exists($_b->blocks['scripts'][] = '_lbc38a422f9f_scripts')) { function _lbc38a422f9f_scripts($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+if (!function_exists($_b->blocks['scripts'][] = '_lbc6cab189da_scripts')) { function _lbc6cab189da_scripts($_b, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
 ?>	<script src="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/js/jquery.js"></script>
 	<script src="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/js/netteForms.js"></script>
 	<script src="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/js/main.js"></script>
@@ -49,11 +49,10 @@ if (empty($_l->extends) && !empty($_control->snippetMode)) {
 	<meta charset="utf-8">
 
 	<title><?php if (isset($_b->blocks["title"])) { ob_start(); Latte\Macros\BlockMacros::callBlock($_b, 'title', $template->getParameters()); echo $template->striptags(ob_get_clean()) ?>
- | <?php } ?>Nette Sandbox</title>
+ | <?php } ?>Nette Adamko</title>
 
-	<link rel="stylesheet" media="screen,projection,tv" href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/screen.css">
-	<link rel="stylesheet" media="print" href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/print.css">
-	<link rel="shortcut icon" href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/favicon.ico">
+	<link rel="stylesheet" media="screen,projection,tv" href="<?php echo Latte\Runtime\Filters::escapeHtml(Latte\Runtime\Filters::safeUrl($basePath), ENT_COMPAT) ?>/css/style.css">
+	
 	<?php if ($_l->extends) { ob_end_clean(); return $template->renderChildTemplate($_l->extends, get_defined_vars()); }
 call_user_func(reset($_b->blocks['head']), $_b, get_defined_vars())  ?>
 
