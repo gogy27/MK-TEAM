0979c4d3f5ee72c7b4c2dac873c71763fa3a8afb
Jan Vodila <jan.vodila@binary-bay.com>
2014-11-18 20:13:13 +0100

    Rozficany login

--- sandbox/app/presenters/HomepagePresenter.php
+++ sandbox/app/presenters/HomepagePresenter.php
@@ -3,60 +3,55 @@
 namespace App\Presenters;
 
 use Nette,
-    App\Model,
-    Nette\Forms\Form;
-;
+		App\Model,
+		Nette\Application\UI\Form;
 
 /**
  * Homepage presenter.
  */
 class HomepagePresenter extends BasePresenter {
 
-    const STUDENT = 'S', TEACHER = 'T';
+	const STUDENT = 'S', TEACHER = 'T';
 
-    private $userManager;
+	private $userManager;
 
-    protected function startup() {
-	parent::startup();
-	$this->userManager = $this->context->authorizator;
-    }
-
-    public function actionDefault() {
-	$user = $this->getUser();
-	if ($user->isLoggedIn()) {
-	    if ($user->isInRole(self::STUDENT)) {
-		$this->redirect('Student:default');
-	    } else if ($user->isInRole(self::TEACHER)) {
-		$this->redirect('Teacher:default');
-	    }
+	protected function startup() {
+		parent::startup();
+		$this->userManager = $this->context->authorizator;
 	}
-    }
 
-    protected function createComponentNewLoginForm() {
-	$form = new Form();
-	$form->addText('name', 'Meno:')
-		->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie meno')
-		->setAttribute('placeholder', 'Zadajte meno');
-	$form->addPassword('password', 'Heslo:')
-		->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie heslo')
-		->setAttribute('placeholder', 'Zadajte heslo');
-	$form->addSubmit('login', 'Prihlásiť');
-	$form->onSuccess[] = array($this, 'newLoginFormSubmitted');
-
-	return $form;
-    }
-
-    public function newLoginFormSubmitted(Form $form) {
-	$user = $this->getUser();
-	$values = $form->getValues();
-	$username = $values['name'];
-	$password = $values['password'];
-	try {
-	    $user->login($username, $password);
-	    $this->redirect('Homepage:default');
-	} catch (Nette\Security\AuthenticationException $e) {
-	    $this->flashMessage($e->getMessage());
+	public function actionDefault() {
+		$user = $this->getUser();
+		if ($user->isLoggedIn()) {
+			if ($user->isInRole(self::STUDENT)) {
+				$this->redirect('Student:default');
+			} else if ($user->isInRole(self::TEACHER)) {
+				$this->redirect('Teacher:default');
+			}
+		}
 	}
-    }
+
+	protected function createComponentNewLoginForm() {
+		$form = new Form;
+		$form->addText('name', 'Meno:')
+						->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie meno')
+						->setAttribute('placeholder', 'Zadajte meno');
+		$form->addPassword('password', 'Heslo:')
+						->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie heslo')
+						->setAttribute('placeholder', 'Zadajte heslo');
+		$form->addSubmit('login', 'Prihlásiť');
+		$form->onSuccess[] = $this->newLoginFormSubmitted;
+
+		return $form;
+	}
+
+	public function newLoginFormSubmitted($form, $values) {
+		try {
+			$this->getUser()->login($values->name, $values->password);
+			$this->redirect('Homepage:default');
+		} catch (Nette\Security\AuthenticationException $e) {
+			$this->flashMessage($e->getMessage());
+		}
+	}
 
 }
