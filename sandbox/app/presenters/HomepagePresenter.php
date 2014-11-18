c61ae32120fa728abec449735f71422de49479ac
Jan Vodila <jan.vodila@binary-bay.com>
2014-11-18 20:18:10 +0100

    Merge templates

--- sandbox/app/presenters/HomepagePresenter.php
+++ sandbox/app/presenters/HomepagePresenter.php
@@ -33,13 +33,20 @@ class HomepagePresenter extends BasePresenter {
 
 	protected function createComponentNewLoginForm() {
 		$form = new Form;
+		$form->getElementPrototype()->class('form-horizontal');
 		$form->addText('name', 'Meno:')
 						->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie meno')
-						->setAttribute('placeholder', 'Zadajte meno');
+						->setAttribute('placeholder', 'Zadajte meno')
+						->setAttribute('class', 'col-sm-4 control-label')
+						->setAttribute('class', 'form-control');
+		$form['name']->getLabelPrototype()->class('col-sm-4 control-label');
 		$form->addPassword('password', 'Heslo:')
 						->addRule(Form::FILLED, 'Musíte zadať svoje prihlasovacie heslo')
-						->setAttribute('placeholder', 'Zadajte heslo');
-		$form->addSubmit('login', 'Prihlásiť');
+						->setAttribute('placeholder', 'Zadajte heslo')
+						->setAttribute('class', 'form-control');
+		$form['password']->getLabelPrototype()->class('col-sm-4 control-label');
+		$form->addSubmit('login', 'Prihlásiť')
+						->setAttribute('class', 'btn btn-primary');
 		$form->onSuccess[] = $this->newLoginFormSubmitted;
 
 		return $form;
