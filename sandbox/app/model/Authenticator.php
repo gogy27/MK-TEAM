129c6e596f6f96c4d7b16a47f974c75b98ebf5c2
Jan Vodila <jan.vodila@binary-bay.com>
2014-12-03 14:43:56 +0100

    pridavanie ziakov do skupiny pri registracii

--- sandbox/app/model/Authenticator.php
+++ sandbox/app/model/Authenticator.php
@@ -23,9 +23,9 @@ class Authenticator extends Nette\Object implements Nette\Security\IAuthenticato
 		$row = $this->database->table(App\Model\UserRepository::TABLE_NAME)->where(App\Model\UserRepository::COLUMN_EMAIL, $email)->fetch();
 
 		if (!$row) {
-			throw new Nette\Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
+			throw new Nette\Security\AuthenticationException('Prihlasovací e-mail je nesprávny.', self::IDENTITY_NOT_FOUND);
 		} elseif (!Passwords::verify($password, $row[App\Model\UserRepository::COLUMN_PASSWORD])) {
-			throw new Nette\Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
+			throw new Nette\Security\AuthenticationException('Vaše heslo je nesprávne.', self::INVALID_CREDENTIAL);
 		} elseif (Passwords::needsRehash($row[App\Model\UserRepository::COLUMN_PASSWORD])) {
 			$row->update(array(
 					App\Model\UserRepository::COLUMN_PASSWORD => Passwords::hash($password),
