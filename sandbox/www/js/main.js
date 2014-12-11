f5a952cf530d73b4198a770135d5ac1d550904e1
Adam Sabik <adam0sabik@gmail.com>
2014-12-11 09:45:15 +0100

    base format autocomplete fix

--- sandbox/www/js/main.js
+++ sandbox/www/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.base-number-format-input').keyup(function () {
-	$(this).parent('.nowrap').siblings('.nowrap').find('.base-number-format').text($(this).val());
+	$(this).siblings('.base-number-format').text($(this).val());
     });
 
     $('a.remove-record').click(function (e) {
